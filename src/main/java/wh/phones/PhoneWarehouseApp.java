a27099d52718a5eb67c83bbc38ff1777ab58b8e8
Cristi ANGHELUTA <cristinel.angheluta@gmail.com>
2023-04-21 09:15:59 +0300

    1st commit - initial app structure generated by start.spring.io

---
+++ src/main/java/wh/phones/PhoneWarehouseApp.java
@@ -0,0 +1,12 @@
+package wh.phones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneWarehouseApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneWarehouseApp.class, args);
+    }
+
+}
